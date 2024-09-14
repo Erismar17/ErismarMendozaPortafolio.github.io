9ba1f34c5df5d390013ced4a837870d6a147ef18
Erismar17 <erismarmendoza.em2005@gmail.com>
2024-09-14 11:45:27 -0500

    menu fixed

--- src/components/header/menu/Menu.jsx
+++ src/components/header/menu/Menu.jsx
@@ -4,18 +4,17 @@ const Menu = ({ isOpen, closeMenu }) => {
     if (!isOpen) return null;
 
     return (
-        <div className='menu-container'>
+        <div className={`menu-container ${isOpen ? '' : 'hidden'}`}>
             <button className='close-menu' onClick={closeMenu}>
                 <span className='material-symbols-rounded'>close</span>
             </button>
-            <div>
-                <p>Technologies</p>
-                <p>Projects</p>
-                <p>CV</p>
-                <p>Contact</p>
-            </div>
+            <ul className='option-menu'>
+                <li><a href='#technologies' onClick={closeMenu}>Technologies</a></li>
+                <li><a href='#projects' onClick={closeMenu}>Projects</a></li>
+                <li><a href='#cv' onClick={closeMenu}>CV</a></li>
+                <li><a href='#contact' onClick={closeMenu}>Contact</a></li>
+            </ul>
         </div>
     );
 };
-
 export default Menu;

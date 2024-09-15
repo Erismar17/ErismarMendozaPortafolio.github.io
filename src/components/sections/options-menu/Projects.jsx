0f7ae16a9972ffb9a24eecbd2f0063047e33280f
Erismar17 <erismarmendoza.em2005@gmail.com>
2024-09-15 16:19:34 -0500

    projects added via link

--- src/components/sections/options-menu/Projects.jsx
+++ src/components/sections/options-menu/Projects.jsx
@@ -1,10 +1,13 @@
-
+import './style.css'
 
 const Projects = () => {
     return (
         <div id='projects' className='projects'>
             <h2>Projects</h2>
-            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
+            <a className='portfolio-projects' href='https://erismar17.github.io/Portfolio-Projects/' target='_blank' rel='noreferrer'>
+                <p>Click here to see the projects I work in to this portfolio</p>
+                <span class='material-symbols-rounded link'>link</span>
+            </a>
         </div>
     )
 }
